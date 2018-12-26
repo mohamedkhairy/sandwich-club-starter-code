8af7526e030c901f45ab219838178d282bfa6a1f
mohamedkhairy <memo2030400@gmail.com>
2018-12-26 14:12:08 +0200

    testbr

---
+++ app/src/main/java/com/udacity/sandwichclub/testActivity.java
@@ -0,0 +1,26 @@
+package com.udacity.sandwichclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        punlic int x = 1;
+
+        punlic int x = 1;
+
+        punlic int x = 1;
+
+        punlic int x = 1;
+
+        punlic int x = 1;
+
+        punlic int x = 1;
+
+    }
+}
